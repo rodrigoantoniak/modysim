6ce2166fe66abd405b742a88bd3d9f4932e64a3b
Rodrigo Lionel Antoniak <rodrigolionelantoniak@gmail.com>
2022-04-21 02:00:35 -0300

    Corrección de método
    Se asegura que las salidas se encuentran entre 0 y 1, mientras que en el vector si se conserva el verdadero resto.

--- congruenciasFundamental/main.cpp
+++ congruenciasFundamental/main.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <string>
 
+double atof(char*);
+
+double itof(int);
+
+int ftoi(double);
+
 int main(int argc, char* argv[]) {
     using namespace std;
     if (argc < 5) {
@@ -32,33 +38,52 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-    int m;
-    int v[argc - 4];
+    double m;
+    int w[argc - 4];
+    double v[argc - 4];
     for (c = 4; c < argc; c++) {
-        v[c - 4] = atoi(argv[c]);
+        w[c - 4] = atoi(argv[c]);
+        v[c - 4] = atof(argv[c]);
     }
     a = atoi(argv[1]);
     c = atoi(argv[2]);
-    m = atoi(argv[3]);
+    m = atof(argv[3]);
     if (a >= m) {
         cout << "Error: el modulo es menor o igual a la primera semilla." << endl;
         return 1;
     }
-    int i, y, z;
+    int i, u, z;
+    double d, y;
     for (i = 0; i < argc - 4; i++) {
-        if (v[i] >= m) {
+        if (w[i] >= ftoi(m)) {
             cout << "Error: el elemento " + to_string(i)
                     + " de la sucesion es mayor o igual al modulo." << endl;
             return 1;
         }
-        y = i + argc - 5;
-        z = y % (argc - 4);
-        y = v[z];
-        z = (a * y + c * v[i]) % m;
-        v[i] = z;
+        z = i + argc - 5;
+        u = z % (argc - 4);
+        y = itof(w[u]);
+        w[i] = (a * w[u] + c * w[i]) % ftoi(m);
+        d = (itof(a) * y + itof(c) * itof(w[i])) / m;
+        while (d >= 1) {
+            d -= 1;
+        }
+        v[i] = d;
     }
     for (i = 0; i < argc - 4; i++) {
         cout << v[i] << endl;
     }
     return 0;
+}
+
+double atof(char* entrada) {
+    return static_cast<double>(atoi(entrada));
+}
+
+double itof(int entrada) {
+    return static_cast<double>(entrada);
+}
+
+int ftoi(double entrada) {
+    return static_cast<int>(entrada);
 }
