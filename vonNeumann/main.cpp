87a9c07a9e004e95ed8081761b12d96174c46a4c
Rodrigo Lionel Antoniak <rodrigolionelantoniak@gmail.com>
2022-04-24 11:28:43 -0300

    Preparación para limitar por rango
    Los programas están escritos de una forma que permita agregar un cierto rango de los números generados con facilidad.

--- vonNeumann/main.cpp
+++ vonNeumann/main.cpp
@@ -4,14 +4,17 @@
 
 int main(int argc, char* argv[]) {
     using namespace std;
+    // Primero, debe controlarse que los argumentos adicionales ingresados sean dos.
     if (argc != 3) {
         cout << "Error: se debe ingresar una semilla y la cantidad de numeros aleatorios a generar." << endl;
         return 1;
     }
-    int n, x;
+    unsigned int n, x;
     string argumento;
     for (x = 1; x < 3; x++) {
+        // Se prefiere el uso de assign antes que '=' para denotar que argv[x] es de un tipo distinto que argumento
         argumento.assign(argv[x]);
+        // Para asegurar que los argumentos son números enteros no negativos, se comprueba que los caracteres sean [0;9]
         for (n = 0; n < argumento.length(); n++) {
             if ((argumento[n] < '0') || (argumento[n] > '9')) {
                 argumento = (x == 1)
@@ -22,34 +25,47 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-    argumento.assign(argv[1]);
-    if (argumento.length() != 4) {
+    x = atoi(argv[1]);
+    n = atoi(argv[2]);
+    if ((x < 1000) || (x > 9999)) {
         cout << "Error: la semilla no es un número natural de cuatro dígitos." << endl;
         return 1;
     }
-    x = atoi(argv[1]);
-    n = atoi(argv[2]);
-    int i, y, z;
+    if (!(n)) {
+        cout << "Error: la cantidad de números aleatorios a generar no puede ser cero." << endl;
+        return 1;
+    }
+    unsigned int i, y;
+    unsigned int v[n];
     for (i = 0; i < n; i++) {
-        while (x % 100 == 0) {
+        // Aquí se comprueba que la semilla no termine en 00
+        // Si es múltiplo de 100, se adiciona un número entre 1 y 99 para cumplir la condición de la semilla
+        if (x % 100 == 0) {
+            // El número a sumar depende de la iteración en que se halle la búsqueda de números pseudoaleatorios
             y = i % 99;
             y += 1;
             x += y;
-            z = x * x;
-            z %= 100;
         }
         y = x * x;
         y %= 1000000;
         y /= 100;
-        for (x = 10000; x > 10; x /= 10) {
-            z = y % x;
-            z *= 10;
-            z /= x;
-            cout << z << ' ';
-        }
-        z = y % 10;
-        cout << z << endl;
+        // Se almacena el número generado dentro de un vector
+        v[i] = y;
+        // Por último, el número generado es la nueva semilla
         x = y;
+    }
+    for (i = 0; i < n; i++) {
+        // Se imprimen cada uno de los dígitos de los números generados, excepto el último dígito
+        for (x = 10000; x > 10; x /= 10) {
+            y = v[i] % x;
+            y *= 10;
+            y /= x;
+            cout << y << ' ';
+        }
+        // Como el último dígito se puede obtener con una operación sencilla, se pone fuera del bucle
+        y = v[i] % 10;
+        // Además, en este programa se realiza un salto de línea para distinguir las cifras cada 4 dígitos
+        cout << y << endl;
     }
     return 0;
 }
