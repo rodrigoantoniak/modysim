5fa509bf0d64e61e6f837e4b5c09c31e8a920441
Rodrigo Lionel Antoniak <rodrigolionelantoniak@gmail.com>
2022-04-25 09:17:36 -0300

    Corrección de verificación de variables
    La verificaciones de cero se realiza correctamente

--- congruenciasFundamental/main.cpp
+++ congruenciasFundamental/main.cpp
@@ -1,89 +1,113 @@
 #include <stdlib.h>
+#include "vonNeumann.hpp"
 #include <iostream>
 #include <string>
 
-double atof(char*);
-
-double itof(int);
-
-int ftoi(double);
-
 int main(int argc, char* argv[]) {
     using namespace std;
-    if (argc < 5) {
-        cout << "Error: se debe ingresar dos semillas fijas, un modulo y una sucesion de numeros aleatorios." << endl;
+    // Primero, debe controlarse que los argumentos adicionales ingresados sean seis.
+    if (argc != 7) {
+        cout << "Error: se debe ingresar los seis argumentos adicionales al programa." << endl;
         return 1;
     }
-    int a, c;
+    unsigned int a, c;
     string argumento;
-    for (c = 1; c < 4; c++) {
+    for (c = 1; c < 7; c++) {
+        // Se prefiere el uso de assign antes que '=' para denotar que argv[x] es de un tipo distinto que argumento
         argumento.assign(argv[c]);
         for (a = 0; a < argumento.length(); a++) {
+            // Para asegurar que los argumentos son números enteros no negativos, se comprueba que los caracteres sean [0;9]
             if ((argumento[a] < '0') || (argumento[a] > '9')) {
-                argumento = (c == 3)
-                    ? "Error: el modulo no es un numero natural."
-                    : "Error: la semilla " + to_string(c) + " no es un numero natural.";
+                switch (c) {
+                    case 3:
+                        argumento = "Error: la cantidad de números anteriores necesarios no es un numero natural.";
+                        break;
+                    case 4:
+                        argumento = "Error: el modulo no es un numero natural.";
+                        break;
+                    case 5:
+                        argumento = "Error: la cantidad de números aleatorios a generar no es un numero natural.";
+                        break;
+                    case 6:
+                        argumento = "Error: la semilla para los números anteriores a generar no es un numero natural.";
+                        break;
+                    default:
+                        argumento = "Error: la semilla " + to_string(c) + " no es un numero natural.";
+                }
                 cout << argumento << endl;
                 return 1;
             }
         }
     }
-    for (c = 4; c < argc; c++) {
-        argumento.assign(argv[c]);
-        for (a = 0; a < argumento.length(); a++) {
-            if ((argumento[a] < '0') || (argumento[a] > '9')) {
-                cout << "Error: el elemento " + to_string(c)
-                    + " de la sucesion no es un numero natural." << endl;
-                return 1;
-            }
-        }
-    }
-    double m;
-    int w[argc - 4];
-    double v[argc - 4];
-    for (c = 4; c < argc; c++) {
-        w[c - 4] = atoi(argv[c]);
-        v[c - 4] = atof(argv[c]);
-    }
+    unsigned int k, m, n, x;
     a = atoi(argv[1]);
     c = atoi(argv[2]);
-    m = atof(argv[3]);
+    k = atoi(argv[3]);
+    m = atoi(argv[4]);
+    n = atoi(argv[5]);
+    x = atoi(argv[6]);
+    if (!(a && c && k && m && n && x)) {
+        cout << "Error: ninguno de los números puede ser igual a cero." << endl;
+        return 1;
+    }
+    unsigned int* v = vonNeumann(k, x);
     if (a >= m) {
         cout << "Error: el modulo es menor o igual a la primera semilla." << endl;
         return 1;
     }
-    int i, u, z;
-    double d, y;
-    for (i = 0; i < argc - 4; i++) {
-        if (w[i] >= ftoi(m)) {
-            cout << "Error: el elemento " + to_string(i)
+    if (k > n) {
+        cout << "Error: la cantidad de números anteriores es mayor a los números aleatorios a generar." << endl;
+        return 1;
+    }
+    if ((x < 1000) || (x > 9999)) {
+        cout << "Error: la semilla para los números anteriores a generar no es un número natural de cuatro dígitos." << endl;
+        return 1;
+    }
+    unsigned int w[n];
+    // Lo siguiente no se realiza dentro del bucle porque obtiene valores distintos a los utilizados
+    // Si bien, es posible ubicarlos dentro del bucle; lo haría menos óptimo al ejecutar
+    if (v[0] >= m) {
+        cout << "Error: el elemento 1 de la sucesion es mayor o igual al modulo." << endl;
+        return 1;
+    }
+    // Se almacena el número generado dentro de un vector
+    w[0] = (a * v[k] + c * v[0]) % m;
+    // Después se realiza las mismas operaciones que se encuentran desde el comentario inmediato anterior
+    // Este bucle trabaja con los k valores anteriores (después de la primera iteración)
+    unsigned int i;
+    for (i = 1; i < k; i++) {
+        if (v[i] >= m) {
+            cout << "Error: el elemento " + to_string(i+1)
                     + " de la sucesion es mayor o igual al modulo." << endl;
             return 1;
         }
-        z = i + argc - 5;
-        u = z % (argc - 4);
-        y = itof(w[u]);
-        w[i] = (a * w[u] + c * w[i]) % ftoi(m);
-        d = (itof(a) * y + itof(c) * itof(w[i])) / m;
-        while (d >= 1) {
-            d -= 1;
-        }
-        v[i] = d;
+        w[i] = (a * w[i-1] + c * v[i]) % m;
     }
-    for (i = 0; i < argc - 4; i++) {
-        cout << v[i] << endl;
+    // Una vez utilizados los k valores anteriores, se utiliza el vector con los valores finales generados
+    for (i = 0; i < n-k; i++) {
+        if (w[k+i-1] >= m) {
+            cout << "Error: el elemento " + to_string(k+i+1)
+                    + " de la sucesion es mayor o igual al modulo." << endl;
+            return 1;
+        }
+        w[k+i] = (a * w[k+i-1] + c * w[i]) % m;
+    }
+    // Por último, se imprimirán los valores aleatorios obtenidos (no cuentan los primeros k valores)
+    // Actualmente, no se agregan ceros a la izquierda de los números enteros
+    // Para esto último, se necesita obtener el número con mayor cantidad de dígitos en un recorrido del vector
+    for (i = 0; i < n; i++) {
+        k = 1;
+        while (k < w[i]) {
+            k *= 10;
+        }
+        for (x = k; x > 10; x /= 10) {
+            m = w[i] % x;
+            m *= 10;
+            m /= x;
+            cout << m << ' ';
+        }
+        m = w[i] % 10;
+        cout << m << endl;
     }
     return 0;
-}
-
-double atof(char* entrada) {
-    return static_cast<double>(atoi(entrada));
-}
-
-double itof(int entrada) {
-    return static_cast<double>(entrada);
-}
-
-int ftoi(double entrada) {
-    return static_cast<int>(entrada);
 }
