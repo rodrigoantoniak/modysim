f5e07060c93afa6058a500f8eda1958d8dd866b5
Rodrigo Lionel Antoniak <rodrigolionelantoniak@gmail.com>
2022-05-11 17:50:19 -0300

    Von Neumann con tests

--- vonNeumann/main.cpp
+++ vonNeumann/main.cpp
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string>
+#include "monobits.hpp"
+#include "chiCuadrado.hpp"
 
 int main(int argc, char* argv[]) {
     using namespace std;
@@ -39,6 +41,14 @@ int main(int argc, char* argv[]) {
     unsigned int v[n];
     for (i = 0; i < n; i++) {
         // Aquí se comprueba que la semilla no termine en 00
+        // Si es un número entre 0 y 99, se adiciona un número entre 1 y 99 (multiplicado por 100) para más aleatoriedad
+        if (x / 100 == 0) {
+            // El número a sumar depende de la iteración en que se halle la búsqueda de números pseudoaleatorios
+            y = 50 - (i % 49);
+            y -= 1;
+            y *= 100;
+            x += y;
+        }
         // Si es múltiplo de 100, se adiciona un número entre 1 y 99 para cumplir la condición de la semilla
         if (x % 100 == 0) {
             // El número a sumar depende de la iteración en que se halle la búsqueda de números pseudoaleatorios
@@ -54,18 +64,32 @@ int main(int argc, char* argv[]) {
         // Por último, el número generado es la nueva semilla
         x = y;
     }
+    unsigned int w[n*4];
+    unsigned int z;
     for (i = 0; i < n; i++) {
         // Se imprimen cada uno de los dígitos de los números generados, excepto el último dígito
+        z = 0;
         for (x = 10000; x > 10; x /= 10) {
             y = v[i] % x;
             y *= 10;
             y /= x;
+            w[(i*4)+z] = y;
             cout << y << ' ';
+            z++;
         }
         // Como el último dígito se puede obtener con una operación sencilla, se pone fuera del bucle
         y = v[i] % 10;
+        w[(i*4)+3] = y;
         // Además, en este programa se realiza un salto de línea para distinguir las cifras cada 4 dígitos
         cout << y << endl;
     }
+    argumento = (monobits(n*4,w))
+        ? "Ha pasado la prueba de monobits."
+        : "No ha pasado la prueba de monobits.";
+    cout << argumento << endl;
+    argumento = (chiCuadrado(n*4,w))
+        ? "Ha pasado la prueba de chi cuadrado."
+        : "No ha pasado la prueba de chi cuadrado.";
+    cout << argumento << endl;
     return 0;
 }
